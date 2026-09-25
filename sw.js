49611bc3677be7ffd9464b968b1ca7449cc2f913
ekataphaundesana-tech <ekataphaundesana@gmail.com>
2026-09-25 11:14:40 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "ekota-foundation-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/logo.jpg",
+  "/hero.jpg",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
